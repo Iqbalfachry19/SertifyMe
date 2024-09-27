3071596bcf0fae560e2e75b12be8bcb0c69c386e
iqbal <ifachry@rocketmail.com>
2024-09-27 17:15:02 +0700

    fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect, FormEvent } from "react";
 import { ethers, Contract } from "ethers";
-
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
