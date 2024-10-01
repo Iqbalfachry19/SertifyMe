8a73b04faa92eb81f5ebcc9d41f7915a6a491128
iqbal <ifachry@rocketmail.com>
2024-10-01 17:21:06 +0700

    fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -498,7 +498,7 @@ export default function App() {
                 className={`${
                   currentView === "home"
                     ? "bg-indigo-50 border-indigo-500 text-indigo-700"
-                    : "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700"
+                    : "border-transparent text-white hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700"
                 } block pl-3 pr-4 py-2 border-l-4 text-base font-medium w-full text-left`}
               >
                 Home
